4dbb39b9d4331d30d5f5520b08446d52db45b2dd
Aadonjo <aadonjomelath@gmail.com>
2024-07-31 14:38:32 +0100

    trackable update

---
+++ Assets/ManagerOutput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.OpenXR;
+
+
+public class ManagerOutput : MonoBehaviour
+{
+
+    private ARMarkerManager m_arMarkerManager;
+    public string outputString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_arMarkerManager = GetComponent<ARMarkerManager>();
+        m_arMarkerManager.markersChanged += OnQRCodesChanged;
+    }
+
+    void OnQRCodesChanged(ARMarkersChangedEventArgs args)
+    {
+        foreach (ARMarker qrCode in args.added)
+            outputString += $"QR code with the ID {qrCode.trackableId} added.";
+
+        foreach (ARMarker qrCode in args.removed)
+            outputString += $"QR code with the ID {qrCode.trackableId} removed.";
+
+        foreach (ARMarker qrCode in args.updated)
+        {
+            outputString += $"QR code with the ID {qrCode.trackableId} updated.";
+            outputString += $"Pos:{qrCode.transform.position} Rot:{qrCode.transform.rotation} Size:{qrCode.size}";
+        }
+    }
+}
